3e1d2bfd4458a42229a563241cd71634198002c8
rdeka <raktimdeka14@gmail.com>
2023-02-28 01:50:29 +0530

    Initial Commit

---
+++ javascript/end.js
@@ -0,0 +1,30 @@
+const username = document.getElementById("username");
+const saveScore = document.getElementById("save-score");
+const finalScore = document.getElementById("finalscore");
+const recentScore = localStorage.getItem("recent-score");
+
+const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
+
+finalScore.textContent = recentScore;
+
+const MaximumNumber_HighScore = 5;
+
+username.addEventListener("keyup", () => {
+  saveScore.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+  console.log("save!");
+  e.preventDefault();
+
+  const score = {
+    score: recentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+  highScores.sort((a, b) => b.score - a.score);
+  highScores.splice(MaximumNumber_HighScore);
+
+  localStorage.setItem("highScore", JSON.stringify(highScores));
+  window.location.assign("../index.html");
+};
---
+++ javascript/game.js
@@ -0,0 +1,80 @@
+const question = document.getElementById("question");
+const choices = Array.from(document.getElementsByClassName("choice-text"));
+const questionCounterText = document.getElementById("questionCounter");
+const scoreText = document.getElementById("score");
+const progressBarFull = document.getElementById("progressBarFull");
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuestions = [];
+
+let questions = [];
+
+fetch("questions.json")
+  .then((res) => res.json())
+  .then((loadedQuestions) => {
+    questions = loadedQuestions;
+    console.log(loadedQuestions);
+    start();
+  })
+  .catch((err) => {
+    console.error(err);
+  });
+
+const CORRECT_BONUS = 5;
+const MAX_QUESTIONS = questions.length;
+
+start = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuestions = [...questions];
+  getNewQuestion();
+};
+
+getNewQuestion = () => {
+  if (availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    localStorage.setItem("recent-score", score);
+    return window.location.assign("../html/end.html");
+  }
+  questionCounter++;
+  questionCounterText.textContent = `${questionCounter}/${MAX_QUESTIONS}`;
+  progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
+  const questionIndex = Math.floor(Math.random() * availableQuestions.length);
+  currentQuestion = availableQuestions[questionIndex];
+  question.textContent = currentQuestion.question;
+
+  choices.forEach((choice) => {
+    const number = choice.dataset["number"];
+    choice.textContent = currentQuestion["choice" + number];
+  });
+
+  availableQuestions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    if (!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
+
+    incrementScore(classToApply === "correct" ? CORRECT_BONUS : 0);
+
+    selectedChoice.parentElement.classList.add(classToApply);
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1200);
+  });
+});
+
+incrementScore = (num) => {
+  score += num;
+  scoreText.textContent = score;
+};
