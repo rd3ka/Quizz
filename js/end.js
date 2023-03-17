49a90a587307b1770c4938e1656faae8722bce8c
rdeka <raktimdeka14@gmail.com>
2023-03-18 02:26:24 +0530

    rebased: end.js

--- js/end.js
+++ js/end.js
@@ -1,18 +1,21 @@
-const username = document.getElementById("username");
-const saveScore = document.getElementById("save-score");
-const finalScore = document.getElementById("finalscore");
-const recentScore = localStorage.getItem("recent-score");
+const username = document.getElementById("username"),
+  saveScore = document.getElementById("save-score"),
+  finalScore = document.getElementById("finalscore"),
+  recentScore = localStorage.getItem("recent-score");
 
 const highScores = JSON.parse(localStorage.getItem("highScore")) || [];
-
+/* Finally Showing Total Score */
 finalScore.textContent = recentScore;
 
+/* Hard coding max no. of highscore */
 const MaximumNumber_HighScore = 5;
 
+/* score cannot be save without a username */
 username.addEventListener("keyup", () => {
   saveScore.disabled = !username.value;
 });
 
+/* storing highScore in localStorage */
 saveHighScore = (e) => {
   console.log("save!");
   e.preventDefault();
